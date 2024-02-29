51afbed959b3fd3a5dbe3b64ea6ffc45e927c79d
sanzamcmillian <g18s2658@ru.ac.za>
2024-02-29 17:53:19 +0200

    up9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,8 +13,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left ? l + binary_tree_height(tree->left) : 0;
-		r = tree->right ? r + binary_tree_height(tree->right) : 0;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > r) ? l : r);
 	}
 	return (0);
