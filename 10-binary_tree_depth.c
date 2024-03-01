72b0a8da961a1cb3f5e4a10c240f551951dd7f12
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-01 05:10:28 +0200

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,13 +9,5 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t size = 0;
-
-	if (tree)
-	{
-		size += 1;
-		size += binary_tree_size(tree->left);
-		size += binary_tree_size(tree->right);
-	}
-	return (size);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
