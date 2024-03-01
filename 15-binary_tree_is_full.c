96e1bfb086e2f1420afac58716652b8fbfc26e07
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-01 06:11:01 +0200

    15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,11 +9,12 @@ int is_full_recursive(const binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		if ((tree->left != NULL && tree->right == NULL) || (tree->left == NULL && tree->right !=NULL) || is_full_recursive(tree->left) == 0 || is_full_recursuve(tree->right) ==0)
+		if ((tree->left != NULL && tree->right == NULL) || (tree->left == NULL && tree->right !=NULL) || is_full_recursive(tree->left) == 0 || is_full_recursive(tree->right) ==0)
 			return (0);
 	}
 	return (1);
 }
+
 /**
  * binary_tree_is_full - Checks if a binary tree is full.
  * @tree: A a pointer to the root node of the tree to check.
@@ -25,6 +26,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	return (is_full_recurive(tree));
+	return (is_full_recursive(tree));
 }
